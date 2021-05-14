75a339b3a852e8ac17cf4e716389a003795af7cf
TcPaulyue <1195882464@qq.com>
2021-05-14 15:28:48 +0800

    refactor

--- src/main/java/info/nemoworks/udo/graphql/dataFetchers/UpdateDocumentMutation.java
+++ src/main/java/info/nemoworks/udo/graphql/dataFetchers/UpdateDocumentMutation.java
@@ -1,55 +1,59 @@
-//package info.nemoworks.udo.graphql.dataFetchers;
-//
-//import com.fasterxml.jackson.databind.JsonNode;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import graphql.schema.DataFetcher;
-//import graphql.schema.DataFetchingEnvironment;
-//import info.nemoworks.udo.model.Udo;
-//import info.nemoworks.udo.service.UdoService;
-//import info.nemoworks.udo.service.UdoServiceException;
-//import info.nemoworks.udo.storage.UdoPersistException;
-//import lombok.SneakyThrows;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.Objects;
-//
-//@Component
-//public class UpdateDocumentMutation implements DataFetcher<JsonNode> {
-//
-//    private final UdoService udoService;
-//
-//    private String documentCollectionName;
-//
-//    public UpdateDocumentMutation(UdoService udoService) {
-//        this.udoService = udoService;
-//    }
-//
-//    public void setDocumentCollectionName(String documentCollectionName) {
-//        this.documentCollectionName = documentCollectionName;
-//    }
-//
-//    @SneakyThrows
-//    @Override
-//    public JsonNode get(DataFetchingEnvironment dataFetchingEnvironment) {
-//        String udoi = dataFetchingEnvironment.getArgument("udoi").toString();
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonNode content = mapper.convertValue(dataFetchingEnvironment.getArgument("content"), JsonNode.class);
-////        Gson gson = new Gson();
-////        JsonObject content = JsonParser.parseString(cont.toJSONString()).getAsJsonObject();
-////        String collection = dataFetchingEnvironment.getArgument("collection").toString();
-//        return Objects.requireNonNull(this.updateDocumentById(udoi, content, documentCollectionName)).getContent();
-//    }
-//
-//    private Udo updateDocumentById(String id, JsonNode content, String collection){
-//        Udo udo = udoService.findUdoById(id, collection);
-//        assert udo != null;
-//        udo.setContent(content);
-//        try {
-//            return udoService.saveOrUpdateUdo(udo);
-//        } catch (UdoServiceException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//
-//}
+package info.nemoworks.udo.graphql.dataFetchers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.internal.LinkedTreeMap;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import info.nemoworks.udo.model.Udo;
+import info.nemoworks.udo.service.UdoService;
+import info.nemoworks.udo.service.UdoServiceException;
+import info.nemoworks.udo.storage.UdoPersistException;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+@Component
+public class UpdateDocumentMutation implements DataFetcher<HashMap<String, LinkedTreeMap>> {
+
+    private final UdoService udoService;
+
+    private String documentCollectionName;
+
+    public UpdateDocumentMutation(UdoService udoService) {
+        this.udoService = udoService;
+    }
+
+    public void setDocumentCollectionName(String documentCollectionName) {
+        this.documentCollectionName = documentCollectionName;
+    }
+
+    @SneakyThrows
+    @Override
+    public HashMap<String, LinkedTreeMap> get(DataFetchingEnvironment dataFetchingEnvironment) {
+        String udoi = dataFetchingEnvironment.getArgument("udoi").toString();
+        JsonObject content = new Gson().fromJson(dataFetchingEnvironment.getArgument("content").toString(), JsonObject.class);
+        String udoTypeId = dataFetchingEnvironment.getArgument("udoTypeId").toString();
+        Udo udo = this.updateDocumentById(udoi, content, documentCollectionName);
+        HashMap hashMap = new Gson().fromJson(udo.getData().toString(), HashMap.class);
+        hashMap.put("udoi",udo.getId());
+        return hashMap;
+    }
+
+    private Udo updateDocumentById(String id, JsonObject content, String collection){
+        Udo udo = udoService.getUdoById(id);
+        assert udo != null;
+        udo.setData(content);
+        try {
+            return udoService.saveOrUpdateUdo(udo);
+        } catch (UdoServiceException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
